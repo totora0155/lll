8c0ba75f05a3e4eb6f037d2729d3ac97a56be277
nju33 <nju33@nmba.local>
2016-03-12 02:33:07 +0900

    Save
    Fix WILL_RENDER
    Implement custom block render
    and so on

--- examples/simple/build.js
+++ examples/simple/build.js
@@ -1,8 +1,17 @@
 const lll = require('../..');
 const marked = require('marked');
+const divide = require('html-divide');
 
 const bases = new lll.Renderer('bases/**/*.html');
 const ports = new lll.Renderer('posts/**/*.md');
+
+bases.on(lll.WILL_RENDER, (content, data) => {
+  const divided = divide(content);
+  if (divided.header) {
+    data.header = divided.header;
+  }
+  return divided.content;
+});
 
 ports.on(lll.WILL_RENDER, (content) => {
   return marked(content);
