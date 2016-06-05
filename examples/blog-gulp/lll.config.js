1e463d6476414fcc3688c503b1e44879bcbc0d0f
nju33 <nju33@nmba.local>
2016-06-05 16:50:24 +0900

    Save

--- examples/blog-gulp/lll.config.js
+++ examples/blog-gulp/lll.config.js
@@ -3,6 +3,13 @@ module.exports = {
   description: 'docs',
   another: 'nju33',
 
+  base: 'src',
+  cloud: {
+    base: 'src',
+    dirname: 'src',
+    parent: 'globalTag'
+  },
+
   separator: '|',
   token: {
     header: '<!-- more -->',
