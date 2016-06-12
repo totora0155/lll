9d7a2f0f7825c915c2b24f98fcb3eb145bf3936f
nju33 <nju33@nmba.local>
2016-06-13 00:04:29 +0900

    Fix

--- examples/N/lll.config.js
+++ examples/N/lll.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   lang: 'ja',
   sitename: 'N',
+  titleToken: ' - ',
   subtitle: 'Noop',
   siteurl: 'http://localhost:3000/',
-  another: 'nju33',
+  author: 'nju33',
 
-  atomId: 'nju33//N',
+  atomId: '5ad93a80-30ad-11e6-ad6c-2731fd8491d9',
 
   base: 'src',
   cloud: {
